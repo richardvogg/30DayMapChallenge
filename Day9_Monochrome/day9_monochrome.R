1c7b247239a956deafe6c8fe4bf2137b4fc3e6cc
richardvogg <46679869+richardvogg@users.noreply.github.com>
2020-11-10 16:13:53 +0100

    added day 10 + botanical garden example

--- Day9_Monochrome/day9_monochrome.R
+++ Day9_Monochrome/day9_monochrome.R
@@ -3,7 +3,6 @@ library(dplyr)
 library(osmdata) #Open Street Map
 
 sysfonts::font_add_google(name = "Crimson Text","Crimson Text")
-sysfonts::font_add_google(name = "Permanent Marker","Permanent Marker")
 showtext::showtext_auto()
 
 # 1. Valparaíso 
@@ -58,3 +57,45 @@ library(patchwork)
 png("Day9_Monochrome/plot.png",width=600,height=500)
 valpo + inset_element(loc,0.4,0.85,0.5,1,align_to = "full")
 dev.off()
+
+
+
+# Botanischer Garten
+
+bbox <- getbb("Würzburg")
+bbox[1,] <- c(9.9287, 9.9347)
+bbox[2,] <- c(49.7634,49.7669)
+
+building <- bbox %>%
+  opq() %>%
+  add_osm_feature(key = "building") %>%
+  osmdata_sf()
+
+wege <- bbox %>%
+  opq() %>%
+  add_osm_feature(key = "highway") %>%
+  osmdata_sf()
+
+water <- bbox %>%
+  opq() %>%
+  add_osm_feature(key = "water") %>%
+  osmdata_sf()
+
+library(ggrepel)
+ggplot(data=building$osm_polygons)+
+  geom_sf(fill="grey70",col="grey20")+
+  
+  geom_sf(data=subset(wege$osm_lines,!highway%in%c("footway","residential")),col="grey",size=1)+
+  geom_sf(data=water$osm_polygons,col="blue")+
+  #geom_sf_text(aes(label=addr.housename),size=3)+
+  xlim(c(9.9287, 9.9336))+ylim(c(49.7634,49.7665))+
+  theme_void()
+
+ggplot()+
+  geom_sf(data = subset(surface$osm_lines,highway=="primary"),col="#bac0c4",size=0.8)+
+  geom_sf(data = subset(surface$osm_lines,highway%in%c("secondary","tertiary")),col="#cbc4b8",size=0.5)+
+  geom_sf(data = subset(surface$osm_lines,!highway%in%c("primary","secondary","tertiary")),col="#d2c1b6")+
+  theme_void()+
+  theme(panel.background = element_rect(colour="#ededed"),
+        plot.title = element_text(family = "Crimson Text", size = 30, colour = "#846d63"),
+        plot.subtitle = element_text(family = "Crimson Text" ,size=20, colour = "#846d63"))
