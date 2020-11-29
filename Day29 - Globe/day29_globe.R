0feee0f36c14797a59592d65e17145c6f7e4ccda
richardvogg <46679869+richardvogg@users.noreply.github.com>
2020-11-29 21:52:01 +0100

    Add day 29

--- Day29 - Globe/day29_globe.R
+++ Day29 - Globe/day29_globe.R
@@ -1,5 +1,19 @@
-library(webglobe)              #Load the library
+library(webglobe)
+library(ggplot2)
+library(dplyr)
 
-wg <- webglobe(immediate=FALSE) #Make a webglobe
-#wg + wgpoints(quakes$lat, quakes$lon, size=5*quakes$mag) #Plot quakes
-wg + wgcamcenter(-24, 178.0650, 8000)                    #Move camera
+sweden <- map_data(map="world",region="Sweden")
+max(sweden$lat)
+median(sweden$long)
+
+
+chile <- map_data(map = "world",region="Chile")
+
+chile_shift <- chile %>%
+  mutate(lat=lat+(max(sweden$lat)-max(chile$lat)),
+         long=long+(median(sweden$long)-median(chile$long)-5))
+
+
+webglobe(immediate=FALSE) + 
+  wgpolygondf(chile_shift,fill="purple",stroke_width = 2) +
+  wgcamcenter(50, 20, 8000)                    #Move camera
